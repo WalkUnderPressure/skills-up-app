acea6bd10fd82517292fb554600362e4d5329f35
WalkUnderPressure <alexwsdaet@gmail.com>
2024-07-27 04:02:53 +0300

    setup webpack for work with html, and add ProgressPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
 
 const BUILD_MODE = process.env.NODE_ENV || 'production'
@@ -10,4 +12,10 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
 };
