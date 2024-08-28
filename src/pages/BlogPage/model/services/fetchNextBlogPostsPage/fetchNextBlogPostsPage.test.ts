e6dcda27e1d2a965257708fb5907304fc40deb5b
WalkUnderPressure <alexwsdaet@gmail.com>
2024-08-28 04:39:18 +0300

    Add simple tests for fetchNextBlogPostsPage

---
+++ src/pages/BlogPage/model/services/fetchNextBlogPostsPage/fetchNextBlogPostsPage.test.ts
@@ -0,0 +1,60 @@
+import { TestAsyncThunk } from 'shared/config/tests/TestAsyncThunk';
+import fetchNextBlogPostsPage from './fetchNextBlogPostsPage';
+import fetchBlogPosts from '../fetchBlogPosts/fetchBlogPosts';
+
+jest.mock('../fetchBlogPosts/fetchBlogPosts');
+
+const initState = {
+  page: 1,
+  ids: [],
+  entities: {},
+  limit: 5,
+  isLoading: false,
+  hasMore: true,
+};
+
+describe('fetchNextBlogPostsPage', () => {
+  test('fetchBlogPosts should be called', async () => {
+    const pageNumber = 5;
+
+    const thunk = new TestAsyncThunk(fetchNextBlogPostsPage, {
+      blogPage: {
+        ...initState,
+        page: pageNumber,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(4);
+    expect(fetchBlogPosts).toHaveBeenCalledWith({ page: pageNumber + 1 });
+  });
+
+  test('fetchBlogPosts should not be called (when hasMore is false)', async () => {
+    const thunk = new TestAsyncThunk(fetchNextBlogPostsPage, {
+      blogPage: {
+        ...initState,
+        hasMore: false,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+    expect(fetchBlogPosts).not.toHaveBeenCalled();
+  });
+
+  test('fetchBlogPosts should not be called (when isLoading is true)', async () => {
+    const thunk = new TestAsyncThunk(fetchNextBlogPostsPage, {
+      blogPage: {
+        ...initState,
+        isLoading: true,
+      },
+    });
+
+    await thunk.callThunk();
+
+    expect(thunk.dispatch).toHaveBeenCalledTimes(2);
+    expect(fetchBlogPosts).not.toHaveBeenCalled();
+  });
+});
