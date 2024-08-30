4f613b9fb8b1ee19d41f08eeaa2ea23a05e78221
WalkUnderPressure <alexwsdaet@gmail.com>
2024-08-31 00:25:05 +0300

    Move json-server to separate repo

--- json-server/lib/randomInArray.ts
+++
@@ -1,7 +0,0 @@
-function randomInArray<T>(values: Array<T>): T | undefined {
-  const index = parseInt(String(Math.random() * values.length));
-
-  return values.at(index) ?? undefined;
-}
-
-export default randomInArray;
